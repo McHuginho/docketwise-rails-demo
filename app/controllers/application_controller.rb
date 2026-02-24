f2bf2c4254cb67044284f851a68b2a0a1a226e1a
Hugo Uribe <hugoalejandrouribe@gmail.com>
2026-02-24 18:03:07 -0500

    Feature: Implemento sistema de seguridad y autenticación con Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,3 +5,7 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 end
+
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_24_020004) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_24_225733) do
   create_table "nota", force: :cascade do |t|
     t.text "contenido"
     t.datetime "created_at", null: false
@@ -25,6 +25,18 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_24_020004) do
     t.string "email"
     t.string "nombre"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "nota", "solicitantes"
